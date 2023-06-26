18154d36e0a7876ef2300db5e29f0952aabc9a30
Braeden Dempsey <braeden.dempsey@icloud.com>
2023-06-26 13:23:24 -0400

    Practicing different implementations of itterative statements.
    - while: validate a user's input
    - for loop: generate a set of even numbers
    - do-while: generate a set of odd numbers

---
+++ Loop_Example_1.cs
@@ -0,0 +1,46 @@
+﻿namespace ISM4300
+{
+    /*
+     *  Lecture 3: Loops
+     */
+    class Loop_Example_1
+    {
+        public static void GetEvenAndOddNumbers()
+        {
+            Console.WriteLine("Enter a positive integer: ");
+            Boolean done = false;
+
+            while (!done)
+            {
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    Console.WriteLine("The input is: " + input);
+
+                    Console.WriteLine("\nList of " + input + " even numbers: ");
+                    for (int i = 0; i < input; i++)
+                    {
+                        int even = 2 * i;
+                        Console.WriteLine(even + " is an even number.");
+                    }
+
+                    Console.WriteLine("\nList of " + input + " odd numbers: ");
+                    int odd = 1;
+                    int counter = 0;
+                    do
+                    {
+                        Console.WriteLine(odd + " is an odd number.");
+                        odd = odd + 2;
+                        counter++;
+                    } while (counter < input);
+
+                    done = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a positive integer.");
+                }
+            }
+        }
+    }
+}
